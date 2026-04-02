f5a01277d214b9349f19cb281465b58ef05c80e6
Dilaray Mercan <dilaray.cam@gmail.com>
2026-04-02 22:31:43 +0300

    app url added

--- src/index.js
+++ src/index.js
@@ -12,4 +12,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// APP URL: "https://fsweb-s10g1-reducer-calculator-raju.onrender.com/"
 reportWebVitals();
